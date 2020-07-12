cdc52c18e9dd0f9ecd13c3be95dab46da971ea38
Hitman <hackzs@outlook.com>
2020-07-12 17:48:09 +0530

    Updates

---
+++ precache-manifest.acc961dd094588940509990ef8f24476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f62d3ea039a6ea0a7d17f8935a3b32c",
+    "url": "/ReactNoteApp/index.html"
+  },
+  {
+    "revision": "f8a507d3e81a0fa6904c",
+    "url": "/ReactNoteApp/static/js/2.bc508148.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/ReactNoteApp/static/js/2.bc508148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c5057b3cfd2d20292e",
+    "url": "/ReactNoteApp/static/js/main.4eda8fe4.chunk.js"
+  },
+  {
+    "revision": "ee1dadf012755311e960",
+    "url": "/ReactNoteApp/static/js/runtime-main.70da07a0.js"
+  }
+]);
